d48779a39b5b6770c92f69262fb05c19b97ed3fa
Sarah <99sarah.s@gmail.com>
2018-01-30 18:59:45 -0700

    Doc

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/ImportantTweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ImportantTweet.java
@@ -1,19 +1,48 @@
+/*ImportantTweet
+ *
+ * Version 1.0
+ *
+ * January 30, 2018
+ * Copyright (c) 2018 Team X CMPUT 301. University of Alberta - All Rights Reserved. You may use distribute or modify this code under terms and condition of the Code of Student Behaviour at University of Alberta.
+ *
+ * You can find a copy of licence in this project. Otherwise please contact contact @abc.ca.
+ */
+
 package ca.ualberta.cs.lonelytwitter;
 
-/**
- * Created by dezfuli on 1/16/18.
- */
 import java.util.Date;
 
+/**
+ * Represents a ImportantTweet
+ *
+ * @author Unknown
+ * @version 1.5
+ * @see Tweet
+ */
 public class ImportantTweet extends Tweet {
+    /**
+     * Constructs importantTweet
+     *
+     * @param message importantTweet message
+     */
     ImportantTweet(String message){
         super(message);
     }
 
+    /**
+     * Constructs importantTweet
+     *
+     * @param message importantTweet message
+     * @param date importantTweet date
+     */
     ImportantTweet(String message, Date date){
         super(message, date);
     }
 
+    /**
+     * Returns boolean to tell it is important.
+     * @return true
+     */
     @Override
     public Boolean isImportant() {
         return Boolean.TRUE;
